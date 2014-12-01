77e8f9e4b8a8ac0a9f42619179d87bf8184b8a00
Quentin Pigné <quentin.pigne@orange.com>
2014-12-01 17:30:50 +0100

    Refactoring SocketIO library. Adding comments. Deleting useless functions. Separating HashMap library

--- SocketIOClient.cpp
+++ SocketIOClient.cpp
@@ -26,116 +26,36 @@
 	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 	OTHER DEALINGS IN THE SOFTWARE.
 */
-#include <SocketIOClient.h>
 
-HashType <char*,void (*)( EthernetClient client, char *data )> SocketIOClient::hashRawArray[HASH_SIZE];
-HashMap  <char*, void(*)( EthernetClient client, char *data )> SocketIOClient::eventHandlers = HashMap<char*,void(*)( EthernetClient client, char *data )>( hashRawArray , HASH_SIZE );	//added
+#include "SocketIOClient.h"
 
-void SocketIOClient::init(int max_events){
-	eventc = 0;	
+//Event handling attributes
+HashType <char*, void(*)(EthernetClient client, char* data)> SocketIOClient::hashRawArray[HASH_SIZE];
+HashMap  <char*, void(*)(EthernetClient client, char* data)> SocketIOClient::eventHandlers = HashMap<char*, void(*)(EthernetClient client, char* data)>(hashRawArray, HASH_SIZE);
+
+//By default constructor, called when a client is instantiated
+SocketIOClient::SocketIOClient() {
+	//At the beginning, the number of handled event is null
+	nbEvent = 0;	
 }
 
-bool SocketIOClient::connect(char thehostname[], int theport) {
-	if (!client.connect(thehostname, theport)) return false;
-	hostname = thehostname;
-	port = theport;
+/*
+	Connection methods
+*/
+
+bool SocketIOClient::connect(char* theHostname, int thePort) {
+	//Ethernet connection as a client, if it fails, socket connection cannot be done
+	if(!client.connect(theHostname, thePort)) return false;
+	hostname = theHostname;
+	port = thePort;
+	//Send handshake to start the socket connection
 	sendHandshake(hostname, port);
+	//Read the handshake's response to know if connection succeed
 	return readHandshake();
 }
 
-bool SocketIOClient::connected() {
-	return client.connected();
-}
-
-void SocketIOClient::disconnect() {
-	client.stop();
-}
-
-// find the nth colon starting from dataptr
-void SocketIOClient::findColon(char which) {	
-	while (*dataptr) {
-		if (*dataptr == ':') {
-			if (--which <= 0) return;
-		}
-		++dataptr;
-	}
-}
-
-// terminate command at dataptr at closing double quote
-void SocketIOClient::terminateCommand(void) {
-	dataptr[strlen(dataptr)-3] = 0;
-}
-
-void SocketIOClient::monitor() {
-
-	*databuffer = 0;
-
-	if (!client.connected()) {
-		if (!client.connect(hostname, port)) return;
-	}
-
-	if (!client.available()) return;
-
-	char which;
-	char *evtnm;
-
-	while (client.available()) {
-		readLine();
-		dataptr = databuffer;
-		switch (databuffer[0]) {	
-
-		case '1':		// connect: []
-			which = 6;
-			break;
-
-		case '2':		// heartbeat: [2::]
-			client.print((char)0);
-			client.print("2::");
-			client.print((char)255);
-			continue;
-
-		case '5':		// event: [5:::{"name":"ls"}]			
-			//event handler pointer
-			void (*evhand)(EthernetClient client, char *data );
-			//Get event name
-			evtnm = getName(databuffer);			
-			if( eventHandlers.getFunction( evtnm , &evhand) ){
-				evhand(client, databuffer);
-			}
-			//uhm...
-			which = 4;
-			break;
-
-		default: 
-			Serial.print("Drop ");
-			Serial.println(dataptr);
-			continue;
-		}
-
-		findColon(which);
-		dataptr += 2;
-
-		// handle backslash-delimited escapes		
-		char *optr = databuffer;
-		while (*dataptr && (*dataptr != '"')) {
-			if (*dataptr == '\\') {
-				++dataptr;		// todo: this just handles "; handle \r, \n, \t, \xdd
-			}
-			*optr++ = *dataptr++;
-		}
-		*optr = 0;
-
-		if (dataArrivedDelegate != NULL) {
-			dataArrivedDelegate(*this, databuffer);
-		}
-	}
-}
-
-void SocketIOClient::setDataArrivedDelegate(DataArrivedDelegate newdataArrivedDelegate) {
-	dataArrivedDelegate = newdataArrivedDelegate;
-}
-
-void SocketIOClient::sendHandshake(char hostname[], int port) {
+void SocketIOClient::sendHandshake(char* hostname, int port) {
+	//Construction of the HTTP request
 	client.println(F("GET /socket.io/1/ HTTP/1.1"));
 	client.print(F("Host: "));
 	client.print(hostname);
@@ -144,131 +64,220 @@ void SocketIOClient::sendHandshake(char hostname[], int port) {
 	client.println(F("Origin: Arduino\r\n"));
 }
 
-bool SocketIOClient::waitForInput(void) {
-unsigned long now = millis();
-	while (!client.available() && ((millis() - now) < 30000UL)) {;}
-	return client.available();
-}
+bool SocketIOClient::readHandshake(){
+	//Check for the server's response
+	if(!waitForInput()) return false;
 
-void SocketIOClient::eatHeader(void) {
-	while (client.available()) {	// consume the header
-		readLine();
-		if (strlen(databuffer) == 0) break;
-	}
-}
-
-bool SocketIOClient::readHandshake() {
-
-	if (!waitForInput()) return false;
-
-	// check for happy "HTTP/1.1 200" response
+	//Check for the "HTTP/1.1 200 OK" response
 	readLine();
-	if (atoi(&databuffer[9]) != 200) {
-		while (client.available()) readLine();
+	if(atoi(&databuffer[9]) != 200) {
+		while(client.available()) readLine();
 		client.stop();
 		return false;
 	}
-	eatHeader();
-	readLine();	// read first line of response
-	readLine();	// read sid : transport : timeout
 
+	eatHeader(); //Consume the rest of the header
+	readLine();	//Consume first line of response (useless data)
+	readLine();	//Read 2nd line (sid : transport : timeout : available_transports)
+
+	//Get the SID in the response
 	char *iptr = databuffer;
 	char *optr = sid;
-	while (*iptr && (*iptr != ':') && (optr < &sid[SID_LEN-2])) *optr++ = *iptr++;
+	while(*iptr && (*iptr != ':') && (optr < &sid[SID_LEN - 2])) *optr++ = *iptr++;
 	*optr = 0;
 
-	Serial.print(F("Connected. SID="));
-	Serial.println(sid);	// sid:transport:timeout 
+	Serial.print(F("Connected. SID = "));
+	Serial.println(sid);
 
-	while (client.available()) readLine();
+	//Consume the rest of the response
+	while(client.available()) readLine();
+
+	//Stop the connection
 	client.stop();
 	delay(1000);
 
-	// reconnect on websocket connection
-	Serial.print(F("WS Connect..."));
-	if (!client.connect(hostname, port)) {
+	//Reconnect on WebSocket connection
+	Serial.print(F("WebSocket Connect... "));
+	if(!client.connect(hostname, port)) {
 		Serial.print(F("Reconnect failed."));
 		return false;
 	}
 	Serial.println(F("Reconnected."));
 
+	//Construction of the protocol switching request
 	client.print(F("GET /socket.io/1/websocket/"));
 	client.print(sid);
 	client.println(F(" HTTP/1.1"));
 	client.print(F("Host: "));
-	client.println(hostname);
+	client.print(hostname);
+	client.print(F(":"));
+	client.println(port);
 	client.println(F("Origin: ArduinoSocketIOClient"));
-	client.println(F("Upgrade: WebSocket"));	// must be camelcase ?!
+	client.println(F("Upgrade: WebSocket"));
 	client.println(F("Connection: Upgrade\r\n"));
 
-	if (!waitForInput()) return false;
+	//Check for the server's response
+	if(!waitForInput()) return false;
 
+	//Check for the "HTTP/1.1 101 Switching Protocols" response
 	readLine();
-	if (atoi(&databuffer[9]) != 101) {
+	if(atoi(&databuffer[9]) != 101) {
 		while (client.available()) readLine();
 		client.stop();
 		return false;
 	}
-	eatHeader();
-	monitor();		// treat the response as input
+
+	eatHeader(); //Consume the rest of the header
+	monitor(); //Treat the response as input
+
 	return true;
 }
 
-void SocketIOClient::readLine() {
-	dataptr = databuffer;
-	while (client.available() && (dataptr < &databuffer[DATA_BUFFER_LEN-2])) {
-		char c = client.read();
-		if (c == 0) Serial.print(F("NULL"));
-		else if (c == 255) Serial.print(F("0x255"));
-		else if (c == '\r') {;}
-		else if (c == '\n') break;
-		else *dataptr++ = c;
+//Server unreachable if it takes more than 30sec to answer
+bool SocketIOClient::waitForInput() {
+	unsigned long now = millis();
+	while(!client.available() && ((millis() - now) < 30000UL)) {}
+	return client.available();
+}
+
+//Consume the header
+void SocketIOClient::eatHeader() {
+	//Consume lines to the empty line between the end of the header and the beginning of the response body
+	while(client.available()) {
+		readLine();
+		if(strlen(databuffer) == 0) break;
 	}
-	*dataptr = 0;
 }
 
-void SocketIOClient::send(char *data) {
-	client.print((char)0);
-	client.print("5:::");
-	client.print(data);
-	client.print((char)255);
+/*
+	Event handling methods
+	Event data format = 5:::{"name":"event_name","args":[]}
+*/
+
+//Map an event name and its handler function
+void SocketIOClient::setEventHandler(char* eventName,  void (*handler)(EthernetClient client, char* data)) {
+	if(nbEvent < HASH_SIZE) eventHandlers[nbEvent++](eventName, handler);
+	else Serial.println('Max number of events reached');
 }
 
-//https://github.com/LearnBoost/socket.io-spec
-void SocketIOClient::sendEvent(char *event, char *data){
+//Event data emitting method
+void SocketIOClient::emit(char* event, char* data) {
 	client.print((char)0);
 	client.print("5:::{\"name\":\"");
 	client.print(event);
 	client.print("\",\"args\":[\"");
 	client.print(data);
 	client.print("\"]}");
-	client.print((char)255);	
+	client.print((char)255);
 }
 
+/*
+	Monitoring for incoming data
+	https://github.com/automattic/socket.io-protocol
+*/
 
-void SocketIOClient::setEventHandler(char* eventName,  void (*handler)(EthernetClient client, char *data )){
-	if( eventc < HASH_SIZE)	eventHandlers[eventc++](eventName,handler);
-	else	Serial.println('MAX NUMBER OF EVENTS REACHED');
+void SocketIOClient::monitor(){
+	*databuffer = 0;
+
+	//If Ethernet client is disconnected, try to reconnect
+	if(!client.connected()) {
+		if(!client.connect(hostname, port)) return;
+	}
+
+	//Stop the method if no data
+	if(!client.available()) return;
+
+	char which;
+
+	while(client.available()) {
+		readLine();
+		dataptr = databuffer;
+		switch(databuffer[0]) {
+			case '1': //connect: [1::]
+				which = 6;
+				break;
+
+			case '2': //heartbeat: [2::]
+				client.print((char)0);
+				client.print("2::");
+				client.print((char)255);
+				continue;
+
+			case '5': //event: [5:::{"name":"event_name","args":[]}]
+				//Get the event name
+				char* evtnm;
+				evtnm = getName(databuffer);
+				//Get the event handler function and call it
+				void (*evhand)(EthernetClient client, char *data );
+				if(eventHandlers.getFunction(evtnm , &evhand)) {
+					evhand(client, databuffer);
+				}
+				//uhm...
+				which = 4;
+				break;
+
+			default:
+				Serial.print("Drop ");
+				Serial.println(dataptr);
+				continue;
+		}
+
+		findColon(which);
+		dataptr += 2;
+
+		//Handle backslash-delimited escapes
+		char *optr = databuffer;
+		while(*dataptr && (*dataptr != '"')) {
+			if(*dataptr == '\\') {
+				++dataptr; //TODO: this just handles "; handle \r, \n, \t, \xdd
+			}
+			*optr++ = *dataptr++;
+		}
+		*optr = 0;
+	}
 }
 
-//		json encoded event
-//		5:::{"name":"event_name","args":[]}
+//Put incoming data's first line into the data buffer
+void SocketIOClient::readLine() {
+	//dataptr pointer points to the beginning of the buffer
+	dataptr = databuffer;
+	//Stop if there is no more data or if reading reaches buffer's max size
+	while(client.available() && (dataptr < &databuffer[DATA_BUFFER_LEN - 2])) {
+		char c = client.read();
+		if(c == 0) {} //Serial.println(F("NULL"));
+		else if(c == 255) {} //Serial.println(F("0x255"));
+		else if(c == '\r') {}
+		else if(c == '\n') break;
+		else *dataptr++ = c;
+	}
+	*dataptr = 0;
+}
+
+//Find the nth colon starting from dataptr
+void SocketIOClient::findColon(char which) {
+	while(*dataptr) {
+		if(*dataptr == ':') {
+			if(--which <= 0) return;
+		}
+		++dataptr;
+	}
+}
+
+//Get the name of the incoming data's event
 char* SocketIOClient::getName(char* databuffer){
-
-	char *nm = databuffer;
-	char *longname;
-
+	char* nm = databuffer;
+	char* longname;
 	int quotes = 0;
 	int beg = 0;
 	int end = 0;
 
-	while( *nm && quotes <  3){ 
-		if( *nm  == '"' ) quotes++;
+	while(*nm && quotes < 3) { 
+		if(*nm  == '"') quotes++;
 		beg++; nm++; 
 	}	
 	longname = nm;
 	end = beg;
-	while( *nm && (*nm  != '"') ){ end++; nm++; }
+	while(*nm && (*nm != '"')) { end++; nm++; }
 
 	char* name = new char[end - beg + 1];
 	strncpy(name, longname, end - beg);
